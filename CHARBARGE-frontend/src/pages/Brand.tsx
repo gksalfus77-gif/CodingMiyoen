5421700e97066d8e76fc0dc0826ba087264d00f2
gksalfus77-gif <gksalfus77@gmail.com>
2026-07-17 14:20:29 +0900

    수정
    수정

--- CHARBARGE-frontend/src/pages/Brand.tsx
+++ CHARBARGE-frontend/src/pages/Brand.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "../css/brand.css";
+import "../css/Brand.css";
 import "../layout/Header.js";
 import "../layout/Footer.js";
 import Footer from "../layout/Footer.js";
